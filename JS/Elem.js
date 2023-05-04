a2c10c8cd1b8cbfa297f7dee05703176559c8d96
DVMaja <majadreilinger@gmail.com>
2023-05-04 14:49:21 +0200

    felvaltva x o -t ír ki

--- JS/Elem.js
+++ JS/Elem.js
@@ -1,6 +1,34 @@
-class Elem{
+class Elem {
+  constructor(szuloElem, index) {
+    this.index = index;
+    //this.divElem = divElem;
+    //this.pElem = pElem;
+    console.log(this.index);
+    //lesz még egy onCLick is
+    console.log(szuloElem);
+    szuloElem.append(`<div class="divElem"><p class="pElem"> </p></div>`);
 
-    constructor(){
-        
-    }
+    this.pElem = $("article .divElem:last-child p");
+    this.divElem = $("article .divElem:last-child");
+    //console.log(this.pElem);
+
+    this.divElem.on("click", () => {
+      //this.setElem.text("X");
+      this.esemenyTrigger();
+    });
+  }
+
+  setElem(ertek) {
+    this.pElem.text(ertek);
+  }
+
+  //saját esemény létrehozása
+  //https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+
+  esemenyTrigger() {
+    const esemeny = new CustomEvent("elemKattintas", {detail: this});    
+    window.dispatchEvent(esemeny);   
+  }
 }
+
+export default Elem;
--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,22 @@
+import Elem from "./Elem.js";
 
+let lepesValtozo = 1;
 
 $(function () {
-    
-})
+  const szuloElem = $(".taroloElem");
+
+  for (let index = 0; index < 9; index++) {
+    const elem = new Elem(szuloElem, index);
+  }
+  $(window).on("elemKattintas", (event) => {
+    console.log(event.detail);
+    let aktElem = event.detail;
+    //aktuális objektumom
+    if (lepesValtozo % 2 == 0) {
+      aktElem.setElem("X");
+    } else {
+      aktElem.setElem("O");
+    }
+    lepesValtozo++;
+  });
+});
